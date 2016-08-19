fb6488624dd189d5938a018a66cca793e18e1a54
Hanefi Onaldi <abdullahanefi16@gmail.com>
2016-08-19 12:22:56 +0300

    changed login behaviour

--- app/app.controller.js
+++ app/app.controller.js
@@ -1,7 +1,10 @@
 class AppController {
   constructor(UserModel) {
   	console.log("constructor")
+  	var vm = this;
   	this.UserModel = UserModel;
+
+  	this.current_user = {}
   }
 
   $onInit() {
@@ -9,11 +12,18 @@ class AppController {
   }
 
   login() {
+  	if (this.current_user.token)
+  	{
+  		console.log("already logged in")
+  		console.log(this.current_user)
+  		return this.current_user
+  	}
   	console.log("tried to login");
   	console.log(this.user);
 
   	this.UserModel.login(this.user).then( (response) => {
   		this.response = response;
+  		this.current_user = response.data.user
   		console.log(this.response);
   	});
   }
